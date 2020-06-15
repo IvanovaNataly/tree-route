1cff5516c0c88f0ea5bc05c59bf76f58dd86b2df
IvanovaNataly <letter4natalia@gmail.com>
2020-06-15 17:33:13 +0300

    init

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+//
+// import { TreeComponent } from './components/tree/tree.component';
+// import { GroupComponent } from './components/group/group.component';
+// import { ItemComponent } from './components/item/item.component';
+
+const routes: Routes = [
+  // { path: '', redirectTo: '/main', pathMatch: 'full' },
+  // { path: 'main', component: TreeComponent },
+  // { path: 'group/:id', component: GroupComponent },
+  // { path: 'item/:id', component: ItemComponent }
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
