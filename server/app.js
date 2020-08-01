59d26aa72c9b274d2a10c5525df18c18dab5028b
IvanovaNataly <letter4natalia@gmail.com>
2020-08-01 21:45:33 +0300

    request with params

--- server/app.js
+++ server/app.js
@@ -1,12 +1,35 @@
 const express = require('express');
 const app = express();
 
+const courses = [
+  { 'id': 1,
+    'name': 'course1'
+  },
+  { 'id': 2,
+    'name': 'course2'
+  },
+  { 'id': 3,
+    'name': 'course3'
+  }
+];
+
 app.get('/', (req, res) => {
   res.send('You are on the base');
 });
 
-app.get('/users', (req, res) => {
-  res.send(JSON.stringify([1, 2, 3]));
+app.get('/api/courses', (req, res) => {
+  res.send(courses);
+});
+
+app.get('/api/course/:id', (req, res) => {
+  const course = courses.find( c => c.id === parseInt(req.params.id));
+
+  if (!course) {
+    res.status(404).send('Item not found...');
+  }
+  else {
+    res.send(course);
+  }
 });
 
 app.listen('3000', () => console.log('listening...'));
